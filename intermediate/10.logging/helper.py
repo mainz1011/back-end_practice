3bea65eee0eb9ca5c6dca23eca8a33c8b1e15256
mainz1011 <mainz1011@hotmail.com>
2024-03-13 23:54:30 +0200

    Delete intermediate/10.logging directory

--- intermediate/10.logging/helper.py
+++
@@ -1,7 +0,0 @@
-import logging
-# below I create my own internal logger (with the name of the module: 'helper')
-logger = logging.getLogger(__name__)
-logger.propagate = False    #if I don't want to have the propagation, I set it to 'False', now it won't propagate to the base logger.
-                             #So now if we run the 'main.py' module, then nothing gets logged here.
-logger.info('hello from helper')
-
